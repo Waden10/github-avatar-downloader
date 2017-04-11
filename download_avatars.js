caa35d66ce3f1be88fc80481246cbd787224f5f5
waden10 <fantm10@hotmail.com>
2017-04-11 18:50:08 +0000

    update

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,16 @@
+var request = require('request');
+const token = require('./token');
+
+
 var GITHUB_USER = "";
 var GITHUB_TOKEN = "";
-var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + 
+repoOwner + '/' + repoName + '/contributors';
 
 
 }
---
+++ token.js
@@ -0,0 +1,6 @@
+var GITHUB_USER = "Waden10";
+var GITHUB_TOKEN = "fcfda1f3dc6571dc4e51609cd7da4545f2a40310";
+
+
+module.exports = GITHUB_TOKEN && GITHUB_USER;
+module.exports = GITHUB_USER;
